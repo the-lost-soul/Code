ac010419f660149127a50bc75723ffc06881080b
the-lost-soul <aquibnazri@gmail.com>
2022-03-18 15:48:04 +0530

    create the sequence starting from 0 and use bitwise operation

---
+++ leetcode/practice/89-gray-code/sol.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n)
+    {
+        vector<int> result ;
+        result.push_back(0) ;
+        for(int i = 0 ; i < n ; i++)
+        {
+            int size = result.size() ;
+            for(int j = size - 1 ; j >= 0 ; j--)
+                result.push_back(result[j] | 1 << i) ;
+        }
+        return result ;
+    }
+};
