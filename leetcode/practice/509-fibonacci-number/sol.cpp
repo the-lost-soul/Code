d11594f91e98c7dc55781a4056e70e518ea2f2d8
the-lost-soul <aquibnazri@gmail.com>
2022-07-28 00:27:54 +0530

    Make sure to handle edge test case for n == 0 .

---
+++ leetcode/practice/509-fibonacci-number/sol.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if(n == 0)
+            return 0 ;
+        vector<int> memo(n + 1 , 0) ;
+        memo[1] = 1 ;
+        for(int i = 2 ; i <= n ; i++)
+            memo[i] = memo[i - 1] + memo[i - 2] ;
+        return memo[n] ;
+    }
+};
